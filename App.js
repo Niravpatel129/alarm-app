b172ee57750a9686ec90d6842801325fc5a81f07
Nirav <niravpatelp129@gmail.com>
2024-02-29 11:47:40 -0500

    Add new files and resources for Android and iOS app

--- App.js
+++ App.js
@@ -1,11 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Notifications from 'expo-notifications';
+import React, { useEffect } from 'react';
+import { Button, Platform, StyleSheet, Text, View } from 'react-native';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function App() {
+  useEffect(() => {
+    const requestPermissions = async () => {
+      try {
+        if (Platform.OS !== 'web') {
+          const { status: existingStatus } = await Notifications.getPermissionsAsync();
+          let finalStatus = existingStatus;
+          if (existingStatus !== 'granted') {
+            const { status } = await Notifications.requestPermissionsAsync();
+            finalStatus = status;
+          }
+          if (finalStatus !== 'granted') {
+            alert('Failed to get push token for push notification!');
+            return;
+          }
+        }
+      } catch (error) {
+        console.log('Error getting a push token', error);
+      }
+    };
+
+    requestPermissions();
+  }, []);
+
+  async function scheduleAlarm() {
+    const schedulingOptions = {
+      content: {
+        title: 'Alarm',
+        body: 'Your alarm is ringing!',
+        sound: 'alarm.mp3',
+        vibrate: [0, 500, 200, 500],
+      },
+      trigger: {
+        seconds: 10,
+      },
+    };
+
+    console.log('Scheduling alarm...');
+    await Notifications.scheduleNotificationAsync(schedulingOptions);
+    console.log('Alarm set for 10 seconds from now.');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Simple Alarm App</Text>
+      <Button title='Set Alarm for 10 Seconds' onPress={scheduleAlarm} />
     </View>
   );
 }
@@ -13,8 +63,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  text: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
   },
 });
