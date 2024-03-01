dc412bf38b707f075bdd687560995491976389ab
Nirav <niravpatelp129@gmail.com>
2024-03-01 12:42:13 -0500

    Remove unused code and update UI in Settings page

--- screens/Settings/Settings.js
+++ screens/Settings/Settings.js
@@ -1,19 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Text, View } from 'react-native';
-import TimePicker from '../../components/TimePicker/TimePicker';
 
 const Settings = () => {
-  const [selectedTime, setSelectedTime] = useState('');
-
-  const handleTimeChange = (time) => {
-    console.log('🚀  time:', time);
-    setSelectedTime(time);
-  };
-
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <TimePicker onTimeChange={handleTimeChange} />
-      <Text>Selected Time: {selectedTime}</Text>
+      <Text>Settings Page</Text>
     </View>
   );
 };
