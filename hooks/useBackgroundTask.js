3c99d6e1be096bb98673c5a10410badfa1859671
Nirav <niravpatelp129@gmail.com>
2024-03-04 16:24:16 -0500

    Refactor useBackgroundTask.js to improve audio handling

--- hooks/useBackgroundTask.js
+++ hooks/useBackgroundTask.js
@@ -21,12 +21,8 @@ const setupTrackPlayer = async () => {
   }
 };
 
-// Plays a very short, silent audio clip at regular intervals to keep the app active
 const playSilentClipAtIntervals = async () => {
-  const intervalDuration = 60000; // For example, every 60 seconds
-
-  // Assuming the silent.mp3 is a very short clip and stored appropriately
-  const { sound } = await Audio.Sound.createAsync(require('../assets/sounds/silent.mp3'), {
+  const { sound } = await Audio.Sound.createAsync(require('../assets/sounds/birds.wav'), {
     shouldPlay: false,
   });
 
@@ -35,30 +31,29 @@ const playSilentClipAtIntervals = async () => {
     staysActiveInBackground: true,
   });
 
-  const intervalId = setInterval(() => {
-    sound.playAsync().then(() => {
-      sound.setPositionAsync(0); // Reset audio position for the next play
-    });
-  }, intervalDuration);
+  sound.playAsync().then(() => {
+    sound.setPositionAsync(0);
+  });
 
   return () => {
-    clearInterval(intervalId);
-    sound.unloadAsync(); // Cleanup the sound instance
+    sound.unloadAsync();
   };
 };
 
 const veryIntensiveTask = async (taskDataArguments) => {
   const { delay } = taskDataArguments;
 
-  await setupTrackPlayer(); // Setup the track player for later use
-  const stopPlayingSilentClip = await playSilentClipAtIntervals(); // Start playing the silent clip at intervals
+  await setupTrackPlayer();
 
-  // Your background task logic goes here
   for (let i = 0; BackgroundService.isRunning(); i++) {
     console.log(`Background iteration: ${i}`);
+
+    if (i % 10 === 0) {
+      playSilentClipAtIntervals();
+    }
+
     if (i === 300 && !backgroundSound) {
-      // Example to play a different sound after some iterations
-      const { sound } = await Audio.Sound.createAsync(require('../assets/sounds/birds2.wav'), {
+      const { sound } = await Audio.Sound.createAsync(require('../assets/sounds/silent.mp3'), {
         shouldPlay: true,
         isLooping: true,
       });
@@ -67,10 +62,8 @@ const veryIntensiveTask = async (taskDataArguments) => {
       sound.playAsync();
     }
 
-    await sleep(delay); // Wait for the specified delay
+    await sleep(delay);
   }
-
-  stopPlayingSilentClip(); // Stop playing the silent clip when not needed
 };
 
 const options = {
