ccaf929501b7f0866e7baa30dbeb7fb56b4caf6c
Nirav <niravpatelp129@gmail.com>
2024-03-04 00:14:49 -0500

    Update app name and slug

--- app.config.js
+++ app.config.js
@@ -1,7 +1,7 @@
 export default {
   expo: {
     name: 'Zen Alarm',
-    slug: 'zen-alarm',
+    slug: 'alarm-app',
     version: '1.0.0',
     orientation: 'portrait',
     icon: './assets/icon.png',
